160d7e182d96c3e8758c1f6ef677bb6256f66fc5
ASUS-PC <hafizjulianda18@gmail.com>
2024-11-11 08:43:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/viewdata/DetailBuahActivity.kt
@@ -0,0 +1,26 @@
+package com.example.viewdata
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.viewdata.databinding.ActivityDetailBuahBinding
+
+class DetailBuahActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBuahBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBuahBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //membuat variabel dan di tangkap data dri intent
+        var namaBuah = intent.getStringExtra("nama")
+        var image = intent.getIntExtra("gambar",0)
+
+        //menangkap data intent dan di set ke layout
+        binding.tvNamaBuah.text = namaBuah
+        binding.imgBuah.setImageResource(image)
+    }
+}
